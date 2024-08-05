f86ee2106f103d3b258f737d818f4af0e5043660
michaldobrzanski <dobrzanski.michal.daniel@gmail.com>
2024-08-05 12:09:21 +0200

    fixed shortest path

--- src/main/java/org/algorithms/maps/RadioStations.java
+++ src/main/java/org/algorithms/maps/RadioStations.java
@@ -3,8 +3,7 @@ package org.algorithms.maps;
 import org.algorithms.AlgorithmCustomData;
 import org.algorithms.BaseAlgorithm;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * You are given a 2D grid representing a map where each cell can either be land or water (m by n).
@@ -14,8 +13,11 @@ import java.util.Queue;
  * Start point is (0,0).
  * End point is (m-1,n-1).
  * <p>
- * Additionally, you need to place radio stations on the grid such that each station
- * can communicate with other stations within a certain distance.
+ * Additionally, you need to place radio stations along the path such that each station
+ * can communicate with other stations within a certain distance of k.
+ * You need to return a list with radio station positions.
+ * A radio station communicates with another one within 1-long radius.
+ * Radio stations can only be built on land cells (labelled as 1).
  * <p>
  * - You can move in 4 directions: up, down, left, and right.
  * - You can only move through land cells.
@@ -37,8 +39,19 @@ public class RadioStations extends BaseAlgorithm<AlgorithmCustomData> {
                 {1, 0, 1, 1, 1}
         };
         int xDest = grid.length - 1, yDest = grid[0].length - 1;
-        final int pathDistance = solve(grid, xDest, yDest);
-        System.out.println("Is there a path? Shortest path distance: " + pathDistance);
+        final List<Point> path = solve(grid, xDest, yDest);
+        System.out.println("Is there a path? Shortest path distance: " + (path.size() - 1));
+        System.out.println("Points along the shortest path:");
+        for (Point point : path) {
+            System.out.println("\t- (" + point.x + ", " + point.y + ")");
+        }
+
+        int minDistance = 2;
+        final List<Point> radioStationPositions = coverArea(grid, minDistance);
+        System.out.println("Placed radio stations at:");
+        for (Point point : radioStationPositions) {
+            System.out.println("\t- (" + point.x + ", " + point.y + ")");
+        }
     }
 
     private record Point(
@@ -46,18 +59,29 @@ public class RadioStations extends BaseAlgorithm<AlgorithmCustomData> {
             int y,
             int distance
     ) {
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
+        }
 
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 
     private final int[] xOffsets = new int[]{0, -1, 0, 1};
     private final int[] yOffsets = new int[]{-1, 0, 1, 0};
 
     private boolean isValid(int[][] grid, int x, int y) {
-        return x >= 0 && x <= grid.length - 1 && y >= 0 && y <= grid[0].length - 1;
+        return x >= 0 && x <= grid.length - 1 && y >= 0 && y <= grid[0].length - 1 && grid[x][y] == 1;
     }
 
-    private int solve(int[][] grid, int xDest, int yDest) {
-        // 1. find a shortest path
+    private List<Point> solve(int[][] grid, int xDest, int yDest) {
+        // 1. find the shortest path
         // 2. place radio stations along the route
 
         boolean[][] visited = new boolean[grid.length][grid[0].length];
@@ -66,11 +90,21 @@ public class RadioStations extends BaseAlgorithm<AlgorithmCustomData> {
         visited[0][0] = true;
         queue.add(start);
 
+        Map<Point, Point> parentMap = new HashMap<>();
+        parentMap.put(start, null);
+
         while (!queue.isEmpty()) {
             final Point point = queue.poll();
 
             if (point.x == xDest && point.y == yDest) {
-                return point.distance;
+                List<Point> shortestPathPoints = new ArrayList<>();
+
+                Point curr = point;
+                while (curr != null) {
+                    shortestPathPoints.add(0, curr);
+                    curr = parentMap.get(curr);
+                }
+                return shortestPathPoints;
             }
 
             for (int i = 0; i < 4; i++) {
@@ -80,11 +114,17 @@ public class RadioStations extends BaseAlgorithm<AlgorithmCustomData> {
                 if (isValid(grid, newX, newY) && !visited[newX][newY]) {
                     // BFS
                     visited[newX][newY] = true;
-                    queue.add(new Point(newX, newY, point.distance + 1));
+                    var p = new Point(newX, newY, point.distance + 1);
+                    queue.add(p);
+                    parentMap.put(p, point);
                 }
             }
         }
-        return -1;
+        return Collections.emptyList();
+    }
+
+    private List<Point> coverArea(int[][] grid, int minimumDistance) {
+        return Collections.emptyList();
     }
 
     @Override
