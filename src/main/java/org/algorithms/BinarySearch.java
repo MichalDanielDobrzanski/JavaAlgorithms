d2b2429032b6d1b9826f67847c0583a408855079
michaldobrzanski <dobrzanski.michal.daniel@gmail.com>
2024-12-26 01:13:55 +0100

    reworked binary search

--- src/main/java/org/algorithms/BinarySearch.java
+++ src/main/java/org/algorithms/BinarySearch.java
@@ -12,40 +12,44 @@ public class BinarySearch extends BaseAlgorithm<ArrayWithIntData> {
     public void execute() {
         int[] data = this.input.first;
         int number = this.input.second;
-//        int index = binarySearch(data, number);
-        int index = binarySearchBroken(data, number);
-        System.out.println("Resulting index is: " + index);
+        int index = binarySearch(data, number);
+        System.out.println("Resulting index for recursive is: " + index);
+        int indexIter = binarySearchIterative(data, number);
+        System.out.println("Resulting index for iterative is: " + indexIter);
     }
 
-    public static int binarySearchBroken(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] < target) {
-                low = mid + 1;
-            } else if (arr[mid] > target) {
-                high = mid - 1;
-            } else {
-                return mid;
-            }
+    int binarySearch(int[] data, int number) {
+        if (data.length == 0) return -1;
+        return search(data, number, 0, data.length - 1);
+    }
+
+    int search(int[] data, int number, int l, int r) {
+        if (l > r) return -1;
+        int mid = (l + r) / 2;
+        if (data[mid] == number) return mid;
+        else if (data[mid] < number) {
+            return search(data, number, mid + 1, r);
+        } else {
+            return search(data, number, l, mid - 1);
         }
-        return -1;
     }
 
-    private int binarySearch(int[] data, int number) {
+    int binarySearchIterative(int[] data, int number) {
         if (data.length == 0) return -1;
         int l = 0;
         int r = data.length - 1;
-        return binarySearchRec(data, l, r, number);
-    }
 
-    private int binarySearchRec(int[] data, int l, int r, int number) {
-        if (l == r) return l;
-        int mid = (r + l) / 2;
-        if (number == data[mid]) return mid;
-        else if (number > data[mid]) return binarySearchRec(data, mid + 1, r, number);
-        else return binarySearchRec(data, l, mid, number);
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (data[mid] == number) return mid;
+            else if (data[mid] < number) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
     }
 
     @Override
