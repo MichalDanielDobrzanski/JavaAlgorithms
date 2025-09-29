436a104dbc1da3bb2254c49b96980addf0c9a636
michaldobrzanski <dobrzanski.michal.daniel@gmail.com>
2025-09-29 08:32:58 +0200

    added cleaner binary search

--- core/src/main/java/org/algorithms/IntegerContainerWithMedian.java
+++ core/src/main/java/org/algorithms/IntegerContainerWithMedian.java
@@ -1,7 +1,6 @@
 package org.algorithms;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,17 +12,31 @@ public class IntegerContainerWithMedian {
         nums = new ArrayList<>();
     }
 
-    // O(log(n))
+    private int binarySearch(int[] arr, int value) {
+        return binSearchRec(arr, value, 0, arr.length - 1);
+    }
+
+    private int binSearchRec(int[] arr, int value, int l, int r) {
+        if (l > r) return -1;
+
+        int mid = (l + r) / 2;
+        if (value == arr[mid]) return mid;
+        else if (value < arr[mid]) return binSearchRec(arr, value, l, mid-1);
+        return binSearchRec(arr, value, mid+1, r);
+    }
+
+
+    // O(n) - because of array resize
     public int add(int value) {
-        int pos = Collections.binarySearch(nums, value);
+        int pos = binarySearch(nums.stream().mapToInt(Integer::intValue).toArray(), value);
         if (pos < 0) pos = -pos - 1;
         nums.add(pos, value);
         return nums.size();
     }
 
-    // O(log(n))
+    // O(n) - because of array resize
     public boolean delete(int value) {
-        int pos = Collections.binarySearch(nums, value);
+        int pos = binarySearch(nums.stream().mapToInt(Integer::intValue).toArray(), value);
         if (pos >= 0) {
             nums.remove(pos);
             return true;
@@ -31,7 +44,7 @@ public class IntegerContainerWithMedian {
         return false;
     }
 
-    // O(1)
+    // O(log(n))
     public Optional<Integer> getMedian() {
         if (nums.isEmpty()) return Optional.empty();
         int n = nums.size();
